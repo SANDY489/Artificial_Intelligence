4d3db97b9b1137701d73f53b8a8b6f58cefccd4f
sandyhuang891 <77770849+sandyhuang891@users.noreply.github.com>
2024-05-21 16:40:07 +0800

    finish finetune

--- NLP_final/task2.2/train_gpt.py
+++ NLP_final/task2.2/train_gpt.py
@@ -1,7 +1,6 @@
 #%%
 import json
 import yaml
-import openai 
 import pandas as pd
 import anthropic # for claude
 from openai import OpenAI #update the latest openai version
@@ -18,7 +17,7 @@ with open("train.json") as f:
 train_list = []
 for item in train_data:
     # sample = {'sentence 1': item[0], 'sentence 2': item[1], 'classification': item[2]}
-    sample = {"messages": [{"role": "system", "content": "You are a NLP researcher. This is an argument relation detection task. You should determine if there is a “Support” relation from sentence 1 to sentence 2 with output 1, else if it is an “Attack” relation from sentence 1 to sentence 2 with output 2, or it does not detect any relationship between sentence 1 and sentence 2 with output 0. Only output one numeric number. "} , 
+    sample = {"messages": [{"role": "system", "content": "You are a NLP researcher. This is an argument relation detection task. You should determine if there is a Supporting relation from sentence 1 to sentence 2 with output 1, else if it is an Attacking relation from sentence 1 to sentence 2 with output 2, or it does not detect any relationship between sentence 1 and sentence 2 with output 0. Only output one numeric number(0,1,2)."} , 
                         {"role": "user", "content": 'Sentence 1: '+item[0]+"\n\n"+'Sentence 2: '+item[1]}, 
                         {"role": "assistant", "content": item[2]}]}
     train_list.append(sample)
@@ -36,16 +35,19 @@ with open("train2.jsonl", 'w', encoding='utf-8') as file:
         json.dump(data, file, ensure_ascii=False)
         file.write(' n')
 
+
 #%% [markdown]
-# Upload training file, use train2.jsonl later
+# Load API key
 from openai import OpenAI
 with open("api_keys.yaml", "r") as file:
     data = yaml.safe_load(file)
 for api_key in data['api_keys']:
     if 'openai_gpt_key' in api_key:
         openai_gpt_key = api_key['openai_gpt_key']
-
 client = OpenAI(api_key = openai_gpt_key)
+
+#%% [markdown]
+# Upload training file, use train2.jsonl later
 client.files.create(
   file=open("train2.jsonl", "rb"),
   purpose="fine-tune"
@@ -53,59 +55,63 @@ client.files.create(
 
 #%% [markdown]
 # Create a fine-tuned model
-#train: FileObject(id='file-4pNFQae9Ew4Qtx7u8e77QYQx'
-# train2: FileObject(id='file-d79KeGrEyG3jZwAwMnzuCEhg', bytes=4249567, created_at=1716231045, filename='train2.jsonl', object='file', purpose='fine-tune', status='processed', status_details=None)
 client.fine_tuning.jobs.create(
-  training_file="file-d79KeGrEyG3jZwAwMnzuCEhg", 
-  model="gpt-3.5-turbo"
+  training_file="file-XKH97TsZDuRY1bbEdRCVhvKP", 
+  model="gpt-3.5-turbo-0125"
 )
 
+
+#%% [markdown]
+# Load test.json
+test_df = pd.read_json('dev.json')
+test_df.head()
+
+#%% [markdown]
+# Create test list
+test_list = []
+for i in range(0, len(test_df)):
+    # sample = {'sentence 1': item[0], 'sentence 2': item[1], 'classification': item[2]}
+    sample = {"messages": [{"role": "system", "content": "You are a NLP researcher. This is an argument relation detection task. You should determine if there is a Supporting relation from sentence 1 to sentence 2 with output 1, else if it is an Attacking relation from sentence 1 to sentence 2 with output 2, or it does not detect any relationship between sentence 1 and sentence 2 with output 0. Only output one numeric number(0,1,2)."} , 
+                        {"role": "user", "content": 'Sentence 1: '+str(test_df.loc[i][0])+"\n\n"+'Sentence 2: '+str(test_df.loc[i][1])}]}
+    test_list.append(sample)
+print(test_list)
+
+#%% [markdown]
+# Create test.jsonl
+with open('dev.jsonl', 'w') as file:
+    for data in test_list:
+        json.dump(data, file)
+        file.write('\n')  
+
+
+#%%
+with open('test.json') as f:
+    data = json.load(f)
+print(data)
 #%% [markdown]
 # Use a fine-tuned model
+# This part not finished
+
 from openai import OpenAI
 client = OpenAI(api_key = openai_gpt_key)
 
-completion = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo:my-org:custom_suffix:id",
-  messages=[
-    {"role": "system", "content": "You are a helpful assistant."},
-    {"role": "user", "content": "Hello!"}
-  ]
-)
-print(completion.choices[0].message)
+for item in data:
+    completion = client.chat.completions.create(
+    model="ft:gpt-3.5-turbo-0125:iis-academia-sinica::9RD2EKa7",
+    messages=[
+        {"role": "system", "content": "You are a NLP researcher. This is an argument relation detection task. You should determine if there is a Supporting relation from sentence 1 to sentence 2 with output 1, else if it is an Attacking relation from sentence 1 to sentence 2 with output 2, or it does not detect any relationship between sentence 1 and sentence 2 with output 0. Only output one numeric number(0,1,2)."},
+        {"role": "user", "content":  'Sentence 1: '+item[0]+"\n\n"+'Sentence 2: '+item[1]}
+    ]
+    )
+    message_to_save = completion.choices[0].message
+    print(message_to_save)
+
+
+    
 
 #%% [markdown]
 # wait
-def load_config(file_path):
-    with open(file_path, "r") as file:
-        data = yaml.safe_load(file)
-    for api_key in data['api_keys']:
-        if 'openai_gpt_key' in api_key:
-            openai_gpt_key = api_key['openai_gpt_key']
-        elif 'claude3_key' in api_key:
-            claude3_key = api_key['claude3_key']
-    return openai_gpt_key, claude3_key
 
 
-
-if __name__ == "__main__":
-
-    #load api keys
-    openai_key, claude3_key = load_config("api_keys.yaml")
-    # print(openai_key, claude3_key)
-
-    #load train, dev file
-    train_data = load_data('train.json')
-    train_list = create_train_list(train_data)
-    # print(train_list)
-
-
-
-
-
-    eval_data = load_data('dev.json')
-    
-    #load openai
-    # openai_api(openai_key)
 
 
